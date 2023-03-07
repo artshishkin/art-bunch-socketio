6fe15abd6172cbde15d6dce8978ef38e47702556
Art <d.art.shishkin@gmail.com>
2023-03-07 12:51:46 +0200

    9.3 WebSocket server (#1)

--- 01-websockets-before-socketio/nativeWs/justWsServer.js
+++ 01-websockets-before-socketio/nativeWs/justWsServer.js
@@ -7,4 +7,10 @@ const server = http.createServer((req, res) => {
     res.end('I am connected!');
 });
 
+//WebSocket Server
+const wss = new websocket.WebSocketServer({
+    //server: server //as it is the same syntax allows use just `server`
+    server
+})
+
 server.listen(8000);
