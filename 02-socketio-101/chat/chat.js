c6c6300516ebdfc04efdf6235f6761dc9ff3b57a
Art <d.art.shishkin@gmail.com>
2023-03-08 15:25:03 +0200

    14.2 Server talks to client (#2)

--- 02-socketio-101/chat/chat.js
+++ 02-socketio-101/chat/chat.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const socketio = require('socket.io')
 
 const app = express();
 
 // app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8000);
+const expressServer = app.listen(8000);
+
+const io = socketio(expressServer);
+
+io.on('connection', (socket) => {
+
+    console.log(socket.id)
+
+    socket.emit("messageFromServer", `Hello from server to ${socket.id}`)
+
+    socket.on('messageToServer', (dataFromClient) => {
+        console.log(`${socket.id} sent me a message ${JSON.stringify(dataFromClient)}`)
+    })
+})
+
