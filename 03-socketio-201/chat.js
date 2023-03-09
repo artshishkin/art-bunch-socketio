cd0a53d40bd527bb28291d1efd6162bf8a3cc6b0
Art <d.art.shishkin@gmail.com>
2023-03-09 23:01:16 +0200

    22.1 Rooms - simplifying project (#3)

--- 03-socketio-201/chat.js
+++ 03-socketio-201/chat.js
@@ -12,32 +12,15 @@ const io = socketio(expressServer);
 
 io.on('connection', (socket) => {
 
-    console.log(`${socket.id} connected to the ${socket.nsp.name} (default) namespace`)
-
     socket.emit("messageFromServer", `Hello from server to ${socket.id}`)
-
     socket.on('messageToServer', (dataFromClient) => {
         console.log(`${socket.id} sent me a message ${JSON.stringify(dataFromClient)}`)
     })
-
-    socket.on('newMessageToServer', (msg) => {
-        console.log(`${socket.id} sent me a message ${JSON.stringify(msg)}`)
-
-        //next lines are equivalent
-        io.of('/').emit('messageToClients', {text: msg.text});
-        // io.emit('messageToClients', {text: msg.text});
-
-        // socket.broadcast.emit('messageToClients', msg); //send to everyone except socket
-    })
-
-    socket.on("pingFromClient", (callback) => {
-        callback();
-    });
-
 })
 
 io.of('/admin').on('connect', (socket) => {
     console.log(`${socket.id} connected to the ${socket.nsp.name} namespace`)
     socket.emit('welcome', 'Welcome to the admin channel!');
+    // io.of('/admin').emit('welcome', 'Welcome to the admin channel!');
 })
 
--- 03-socketio-201/public/scripts.js
+++ 03-socketio-201/public/scripts.js
@@ -2,13 +2,6 @@ const socket = io('http://localhost:8000'); //the / namespace/endpoint
 const socket2 = io('http://localhost:8000/admin'); //the /admin namespace
 // const socket2 = io('http://localhost:8000/marketing'); //the /marketing namespace
 
-socket.on('connect', () => {
-    console.log(`Socket default: ${socket.id}`);
-})
-socket2.on('connect', () => {
-    console.log(`Socket /admin: ${socket2.id}`);
-})
-
 socket2.on('welcome', (msg) => {
     console.log(msg)
 })
@@ -22,10 +15,5 @@ $('#message-form').on('submit', (event) => {
     event.preventDefault();
     const newMessage = $('#user-message').val();
     // console.log(newMessage)
-    socket.emit('newMessageToServer', {text: newMessage});
-})
-
-socket.on('messageToClients', (msg) => {
-    console.log(msg)
-    $('#messages').append(`<li>${msg.text}</li>`);
+    socket.emit('messageToServer', {text: newMessage});
 })
