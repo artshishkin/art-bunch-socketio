3e54fdb045dbec63b0053eb4930050361a12ed6e
Art <d.art.shishkin@gmail.com>
2023-03-14 22:28:39 +0200

    38.2 npm install helmet --save (#5)

--- 04-multiplayer-game/server.js
+++ 04-multiplayer-game/server.js
@@ -6,4 +6,6 @@ const socketio = require('socket.io')
 const expressServer = app.listen(8080);
 
 const io = socketio(expressServer);
+const helmet = require('helmet');
+app.use(helmet());
 console.log("Express and socketio are listening on port 8080");
