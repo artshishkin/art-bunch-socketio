27ec4d7f084f89a7ad545a3940094f08aa2edd94
Art <d.art.shishkin@gmail.com>
2023-03-15 18:24:36 +0200

    39.2 Getting the DOM setup (#5)

---
+++ 04-multiplayer-game/public/uiStuff.js
@@ -0,0 +1,28 @@
+const wHeight = $(window).height();
+const wWidth = $(window).width();
+let player = {}
+
+const canvas = $('#the-canvas');
+const context = canvas[0].getContext('2d');
+canvas.width(wWidth);
+canvas.height(wHeight);
+
+$(window).load(() => {
+    $('#loginModal').modal('show');
+})
+
+$('.name-form').submit((event) => {
+    event.preventDefault();
+    player.name = $('#name-input').val();
+    $('#loginModal').modal('hide');
+    $('.player-name').text(player.name);
+    $('#spawnModal').modal('show');
+})
+
+$('#play-solo-btn').click(() => {
+    $('#spawnModal').modal('hide');
+    console.log('play')
+    $('.hiddenOnStart').removeAttr('hidden');
+    init();
+})
+
