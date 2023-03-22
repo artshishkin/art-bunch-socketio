adf6da8d395289f491c55dde0e791b665080d7e5
Art <d.art.shishkin@gmail.com>
2023-03-22 18:08:28 +0200

    55. Getting performance data (#7)

--- 05-react-socketio/nodeClient/index.js
+++ 05-react-socketio/nodeClient/index.js
@@ -3,3 +3,36 @@
 // Req:
 // - farmhash
 // - socket.io-client
+
+const os = require('os');
+
+const pData = getMachineData()
+
+console.log(pData)
+
+function getMachineData() {
+
+// - CPU load (current)
+// - Memory Usage
+//   - free
+//   - total
+// - Operating System (OS Type)
+// - Time Online (uptime)
+// - Processor Information (CPU info)
+//   - Type
+//   - Number of Cores
+//   - Clock Speed
+
+    const osType = os.type() === 'Darwin' ? 'Mac' : os.type();
+
+    return {
+        osType,
+        freeMem: os.freemem(),
+        totalMem: os.totalmem(),
+        memUsage: Math.floor(100 * (1 - os.freemem() / os.totalmem())) / 100,
+        uptime: os.uptime(),
+        cpuModel: os.cpus()[0].model,
+        cpuSpeed: os.cpus()[0].speed,
+        numCores: os.cpus().length
+    };
+}
