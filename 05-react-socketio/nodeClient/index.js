a9680cc7dc92498af2df85ef4b356451289a9bcf
Art <d.art.shishkin@gmail.com>
2023-03-22 19:27:55 +0200

    56.1 CPU Load calculation (#7)

--- 05-react-socketio/nodeClient/index.js
+++ 05-react-socketio/nodeClient/index.js
@@ -9,6 +9,23 @@ const os = require('os');
 const pData = getMachineData()
 
 console.log(pData)
+let prevCpuLoad = {};
+setInterval(() => {
+
+    const cpuLoad = cpuAverage();
+
+    if (prevCpuLoad.totalMs) {
+        const deltaLoad = {
+            idleDelta: cpuLoad.idleMs - prevCpuLoad.idleMs,
+            totalDelta: cpuLoad.totalMs - prevCpuLoad.totalMs
+        }
+        const cpuLoadParam = 100 - Math.floor(100 * deltaLoad.idleDelta / deltaLoad.totalDelta);
+        console.log(cpuLoadParam);
+    }
+
+    prevCpuLoad = cpuLoad;
+}, 1000)
+
 
 function getMachineData() {
 
@@ -34,5 +51,28 @@ function getMachineData() {
         cpuModel: os.cpus()[0].model,
         cpuSpeed: os.cpus()[0].speed,
         numCores: os.cpus().length
+    };
+}
+
+// cpu is all cores. we need the average of all the cores which will give us the cpu average
+function cpuAverage() {
+    // get ms in each mode, BUT this number is since reboot
+    // so get it now, and get it in 100ms and compare
+    let idleMs = 0;
+    let totalMs = 0;
+    // loop through all the cores
+    os.cpus()
+        .forEach((aCore) => {
+            for (type in aCore.times) {
+                totalMs += aCore.times[type];
+            }
+            idleMs += aCore.times.idle;
+        });
+    const cores = os.cpus().length;
+    const idleAvgMs = idleMs / cores;
+    const totalAvgMs = totalMs / cores;
+    return {
+        idleMs: idleAvgMs,
+        totalMs: totalAvgMs
     };
 }
